cfda2abb886b79d291d8e7a20710f77183e6b627
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2022-04-11 12:13:15 +0200

    first try for native tests

---
+++ src/test/java/com/planner/api/controller/NativeQueryControllerIT.java
@@ -0,0 +1,11 @@
+package com.planner.api.controller;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+/**
+ * Guide: https://quarkus.io/guides/building-native-image#testing-the-native-executable
+ */
+@NativeImageTest
+public class NativeQueryControllerIT extends QueryControllerTest {
+    // Run the same tests
+}
